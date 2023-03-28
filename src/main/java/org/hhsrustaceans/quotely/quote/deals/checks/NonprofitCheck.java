075052eec16053918fbc0ba93e3e8525fd088fa1
Powerbyte7 <joranklaui@gmail.com>
2023-03-28 15:07:14 +0200

    Add non-profit discount

---
+++ src/main/java/org/hhsrustaceans/quotely/quote/deals/checks/NonprofitCheck.java
@@ -0,0 +1,21 @@
+package org.hhsrustaceans.quotely.quote.deals.checks;
+
+import org.hhsrustaceans.quotely.quote.client.Client;
+import org.hhsrustaceans.quotely.quote.client.ClientType;
+import org.hhsrustaceans.quotely.quote.deals.Deal;
+import org.hhsrustaceans.quotely.quote.deals.DealPercentage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NonprofitCheck implements DealCheck {
+    public List<Deal> getDeals(Client client) {
+        List<Deal> deals = new ArrayList<>();
+
+        if (client.getType().equals(ClientType.NONPROFIT)) {
+            deals.add(new DealPercentage("Nonprofit discount", 10.0));
+        }
+
+        return deals;
+    }
+}
