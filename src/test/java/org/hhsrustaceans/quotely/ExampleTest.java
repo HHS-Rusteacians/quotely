dcb5513f50cf28d1e2ac28025e4ab87e037f7f83
Kees van Voorthuizen <me@keesvv.nl>
2023-03-30 13:28:07 +0200

    test: add ExampleTest

---
+++ src/test/java/org/hhsrustaceans/quotely/ExampleTest.java
@@ -0,0 +1,11 @@
+package org.hhsrustaceans.quotely;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    public void testExample() {
+        Assertions.assertEquals(4, 2 + 2);
+    }
+}
