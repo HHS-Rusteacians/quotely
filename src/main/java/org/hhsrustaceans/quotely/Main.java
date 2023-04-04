764b437bbe2c34ccd277c1a249f75e589eb33ce9
Kees van Voorthuizen <me@keesvv.nl>
2023-04-04 10:00:38 +0200

    feat: implement picocli

--- src/main/java/org/hhsrustaceans/quotely/Main.java
+++ src/main/java/org/hhsrustaceans/quotely/Main.java
@@ -1,32 +1,11 @@
 package org.hhsrustaceans.quotely;
 
-import org.hhsrustaceans.quotely.output.OutputWriter;
-import org.hhsrustaceans.quotely.output.AsciiWriter;
-import org.hhsrustaceans.quotely.quote.component.Component;
-import org.hhsrustaceans.quotely.quote.deals.DealManager;
-import org.hhsrustaceans.quotely.quote.options.Category;
-import org.hhsrustaceans.quotely.quote.options.Option;
-import org.hhsrustaceans.quotely.quote.Quote;
-import org.hhsrustaceans.quotely.quote.component.OptionComponent;
-import org.hhsrustaceans.quotely.quote.component.PriceComponent;
+import org.hhsrustaceans.quotely.cli.commands.CommandRoot;
+import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
-        Quote quote = new Quote();
-        Category essentials = new Category("peripherals", "Peripherals");
-        DealManager dealManager = new DealManager(quote.getClient());
-
-        quote.addComponent(new PriceComponent("Omhulsing", 20000.0));
-        quote.addComponent(new PriceComponent("Motor", 3000.0));
-
-        Component testComponent = new PriceComponent("Keuken", 2000.0);
-        dealManager.applyDeals(testComponent);
-        quote.addComponent(testComponent);
-
-        quote.addComponent(new OptionComponent(new Option("Koelkast", 1200.0, essentials)));
-        quote.addComponent(new OptionComponent(new Option("Vaatwasser", 800.0, essentials, true)));
-
-        OutputWriter writer = new AsciiWriter();
-        writer.write(System.out, quote);
+        int statusCode = new CommandLine(new CommandRoot()).execute(args);
+        System.exit(statusCode);
     }
 }
---
+++ src/main/java/org/hhsrustaceans/quotely/cli/commands/CommandRoot.java
@@ -0,0 +1,47 @@
+package org.hhsrustaceans.quotely.cli.commands;
+
+import org.hhsrustaceans.quotely.output.AsciiWriter;
+import org.hhsrustaceans.quotely.output.OutputWriter;
+import org.hhsrustaceans.quotely.quote.Quote;
+import org.hhsrustaceans.quotely.quote.component.Component;
+import org.hhsrustaceans.quotely.quote.component.OptionComponent;
+import org.hhsrustaceans.quotely.quote.component.PriceComponent;
+import org.hhsrustaceans.quotely.quote.deals.DealManager;
+import org.hhsrustaceans.quotely.quote.options.Category;
+import org.hhsrustaceans.quotely.quote.options.Option;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(
+    name = "quotely",
+    description = "A simple and efficient quotation generation system that scales.",
+    mixinStandardHelpOptions = true
+)
+public class CommandRoot implements Callable<Integer> {
+    @Command(name = "generate", description = "Generate a quote.")
+    public void generate() {
+        Quote quote = new Quote();
+        Category essentials = new Category("peripherals", "Peripherals");
+        DealManager dealManager = new DealManager(quote.getClient());
+
+        quote.addComponent(new PriceComponent("Omhulsing", 20000.0));
+        quote.addComponent(new PriceComponent("Motor", 3000.0));
+
+        Component testComponent = new PriceComponent("Keuken", 2000.0);
+        dealManager.applyDeals(testComponent);
+        quote.addComponent(testComponent);
+
+        quote.addComponent(new OptionComponent(new Option("Koelkast", 1200.0, essentials)));
+        quote.addComponent(new OptionComponent(new Option("Vaatwasser", 800.0, essentials, true)));
+
+        OutputWriter writer = new AsciiWriter();
+        writer.write(System.out, quote);
+    }
+
+    @Override
+    public Integer call() {
+        System.err.println("No command specified");
+        return 1;
+    }
+}
