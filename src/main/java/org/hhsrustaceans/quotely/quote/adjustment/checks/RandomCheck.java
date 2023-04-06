f075e5e8b5b20c4e6ce355e960f90beb964ca6c6
Powerbyte7 <joranklaui@gmail.com>
2023-04-06 13:23:01 +0200

    Flip percentages in randomcheck for new math

--- src/main/java/org/hhsrustaceans/quotely/quote/adjustment/checks/RandomCheck.java
+++ src/main/java/org/hhsrustaceans/quotely/quote/adjustment/checks/RandomCheck.java
@@ -13,9 +13,9 @@ public class RandomCheck implements AdjustmentCheck {
         List<ValueAdjustment> deals = new ArrayList<>();
 
         if (Math.random() < 0.5) {
-            deals.add(new AdjustmentPercentage("Lucky lucky!", 70.0));
+            deals.add(new AdjustmentPercentage("Lucky lucky!", -70.0));
         } else {
-            deals.add(new AdjustmentPercentage("Pandemic inflation", -20.0));
+            deals.add(new AdjustmentPercentage("Pandemic inflation", 20.0));
         }
 
         return deals;
