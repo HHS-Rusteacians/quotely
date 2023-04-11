3ce44fe4067b651a45d7356b39ae1df5341ddd3f
CasperThePhantom <c.baijings@gmail.com>
2023-04-11 14:40:46 +0200

    jsr310 dependency and Yaml class added

---
+++ src/main/java/Yaml/Yaml.java
@@ -0,0 +1,9 @@
+package Yaml;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class Yaml {
+
+}
