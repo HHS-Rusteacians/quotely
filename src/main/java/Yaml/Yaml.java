e5df6ed36da1dd78ca850666ca5b20717c5e56ad
CasperThePhantom <c.baijings@gmail.com>
2023-04-13 13:29:15 +0200

    Yaml template edited + Yaml start but still giving errors

--- src/main/java/Yaml/Yaml.java
+++ src/main/java/Yaml/Yaml.java
@@ -1,9 +1,14 @@
 package Yaml;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.hhsrustaceans.quotely.quote.Quote;
 import java.io.File;
 import java.io.IOException;
 
 public class Yaml {
-
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+        Quote order = mapper.readValue(new File("examples/template.yaml"), Quote.class);
+    }
 }
