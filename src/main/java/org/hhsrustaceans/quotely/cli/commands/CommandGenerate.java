c5d0b41d725fd9750a3b0e59134dc8d3e9c9b3af
RedIsGaming <jaredhofman@protonmail.com>
2023-04-14 15:28:40 +0200

    docs: added comments to commands folder

--- src/main/java/org/hhsrustaceans/quotely/cli/commands/CommandGenerate.java
+++ src/main/java/org/hhsrustaceans/quotely/cli/commands/CommandGenerate.java
@@ -15,17 +15,44 @@ import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 
+/**
+ * Creates a command that generates a quote.
+ * @Command(name = "generate", description = "Generate a quote.")
+ * @see picocli.CommandLine.Command;
+ * It contains 2 arguments that is used for setting the quote value.
+ */
 @Command(name = "generate", description = "Generate a quote.")
 public class CommandGenerate implements Runnable {
+
+    /**
+     * @CommandLine.Option(names = "-N", description = "The name of the client.", required = true)
+     * @see picocli.CommandLine.Option;
+     * It is used for setting the name of the client.
+     */
     @CommandLine.Option(names = "-N", description = "The name of the client.", required = true)
     private String clientName;
 
+    /**
+     * @CommandLine.Option(names = "--currency", description = "The currency to use for the quote.")
+     * @see picocli.CommandLine.Option;
+     * It is used for setting the currency of the quote.
+     */
     @CommandLine.Option(names = "--currency", description = "The currency to use for the quote.")
     private Currency currency;
 
+    /**
+     * @CommandLine.ArgGroup(exclusive = false, multiplicity = "0..*")
+     * @see picocli.CommandLine.ArgGroup;
+     * It is used for setting the category of the quote.
+     */
     @CommandLine.ArgGroup(exclusive = false, multiplicity = "0..*")
     private List<Category> categories;
 
+    /**
+     * @CommandLine.ArgGroup(exclusive = false, multiplicity = "0..*")
+     * @see picocli.CommandLine.ArgGroup;
+     * It is used for setting the options of the quote.
+     */
     @CommandLine.ArgGroup(exclusive = false, multiplicity = "0..*")
     private List<Option> options;
 
@@ -34,6 +61,11 @@ public class CommandGenerate implements Runnable {
         this.options = new ArrayList<>();
     }
 
+    /**
+     * Creates a quote and sets the client name, currency and options.
+     * @Override annotation is used to override the run method.
+     * It contains a for loop that is used for setting the category of the quote.
+     */
     @Override
     public void run() {
         Quote quote = new Quote();
@@ -43,6 +75,9 @@ public class CommandGenerate implements Runnable {
             quote.setCurrency(currency);
         }
 
+        // A for loop that iterates through the options and checks if the category is not null.
+        // If the category is not null, it will set the category of the option.
+        // Then there is a Category stream that will filter the categories and find the first category.
         for (Option option : options) {
             if (option.getCategory() != null) {
                 Category foundCategory = categories.stream()
@@ -52,14 +87,18 @@ public class CommandGenerate implements Runnable {
 
                 option.setCategory(foundCategory);
             }
+
+            // It will add the option to the quote.
             quote.addComponent(new OptionComponent(option));
         }
 
         AdjustmentManager adjustmentManager = new AdjustmentManager(quote);
+
         for (Component component : quote.getComponents()) {
             adjustmentManager.applyAdjustments(component);
         }
 
+        // It will create a new AsciiWriter and write the quote to the console.
         OutputWriter writer = new AsciiWriter();
         writer.write(System.out, quote);
     }
--- src/main/java/org/hhsrustaceans/quotely/cli/commands/CommandRoot.java
+++ src/main/java/org/hhsrustaceans/quotely/cli/commands/CommandRoot.java
@@ -5,6 +5,19 @@ import picocli.CommandLine.Spec;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.ParameterException;
 
+/**
+ * Creates a command that is used to generate a quote.
+ *
+ * It contains 5 arguments that is used for setting the quote value for the client.
+ * @Command annotation is used to create a command that is used to generate a quote.
+ * @see picocli.CommandLine.Command;
+ *
+ * name = "quotely" is used to set the name of the command.
+ * description = "A simple and efficient quotation generation system that scales." is used to set the description of the command.
+ * mixinStandardHelpOptions = true is used to set the help option.
+ * synopsisSubcommandLabel = "<COMMAND>" is used to set the subcommand label.
+ * subcommands = { CommandGenerate.class } is used to set the subcommands.
+ */
 @Command(
     name = "quotely",
     description = "A simple and efficient quotation generation system that scales.",
@@ -14,9 +27,20 @@ import picocli.CommandLine.ParameterException;
         CommandGenerate.class
     }
 )
+
 public class CommandRoot implements Runnable {
+    /**
+     * @Spec CommandSpec spec;
+     * @see picocli.CommandLine.Spec;
+     * It is used to set the command specification.
+     */
     @Spec CommandSpec spec;
 
+    /**
+     * @Override annotation is used to override the run method.
+     * @throws ParameterException if the subcommand is missing.
+     * It contains a throw statement that throws an exception if the subcommand is missing.
+     */
     @Override
     public void run() {
         throw new ParameterException(spec.commandLine(), "Missing required subcommand");
